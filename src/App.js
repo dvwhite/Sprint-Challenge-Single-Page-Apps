3ce43f4292d28c7188817ff418d729a5aafbe45e
David White <dwhite0627@gmail.com>
2020-01-24 20:42:03 -0500

    Hook up the Welcome and Characters routes, add link to /Characters to the Welcome page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from "react";
 import Header from "./components/Header.js";
 import CharacterList from './components/CharacterList';
+import { Route } from 'react-router-dom';
+import WelcomePage from './components/WelcomePage';
 
 export default function App() {
   return (
     <main>
       <Header />
-      <CharacterList />
+      <Route 
+        exact
+        path='/'
+        component={WelcomePage}
+      />
+      <Route 
+        exact
+        path='/Characters'
+        component={CharacterList}
+      />
     </main>
   );
 }
