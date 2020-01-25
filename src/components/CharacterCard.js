4f3d0adce0773a83e2f0fbb30b8fd5d3c115d70d
David White <dwhite0627@gmail.com>
2020-01-24 19:50:23 -0500

    Create the CharacterCard component, add it to the CharacterList component api data mapping

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,21 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard({character}) {
+  return (
+    <div className="character-card">
+      <div>
+        <img src={character.image} alt={character.name}/>
+      </div>
+      <h2>{character.name}</h2>
+      <div className="character-info">
+        Species: <em>{character.species}</em>
+      </div>
+      <div className="character-info">
+        Origin: <strong>{character.origin.name}</strong>
+      </div>
+      <div className="character-info">
+        Created: <strong>{character.created}</strong>
+      </div>
+    </div>
+  );
 }
